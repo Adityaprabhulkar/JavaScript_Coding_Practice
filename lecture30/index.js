2371d2ea592e7a3d3e21a8d911deb143bf459297
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-10-23 01:09:09 +0530

    lecture 30 get added and updated

---
+++ lecture30/index.js
@@ -0,0 +1,96 @@
+// console.log("Aaditya");
+
+
+
+// Function Defination
+// function sayMyName() {
+//     console.log("Love Babbar");
+    
+// }
+
+
+// function use-Function Call
+// sayMyName();
+
+
+// function printCounting() {
+//     for ( let i = 1; i <= 100; i++) {
+//         console.log(i);
+        
+//     }
+// }
+
+// printCounting();
+
+// function printNumber(num) {
+//     console.log("Printing Number:", num);
+    
+// }
+
+// printNumber(100)
+
+
+// function letAvarage(a, b) {
+//     let avg = (a + b)/2;
+//     console.log("Avarage:", avg);
+    
+// }
+
+// letAvarage(10, 220);
+
+// function getSum(a,b,c) {
+//     let sum  = a + b + c;
+//     return sum;
+// }
+
+// let ans = getSum(1,2,3); 
+// console.log( "Printing Ans:", ans);
+
+
+// function getMyName(firstName, lastName) {
+//     let fullName = firstName  + lastName;
+//     return fullName;
+// // unreachable statemments
+// // let a = 10;
+// // let b = 20;
+// // let c = a + b;
+// }
+
+// let fullName = getMyName(7, 23);
+// console.log("Full Name:-", fullName);
+
+// function getMultiply(a,b) {
+//     return a * b;
+// }
+
+// console.log(getMultiply(2, 5));
+
+
+// const getMultiply = function (a,b) {
+//     return a * b;
+// }
+
+// let ans = getMultiply(3, 4);
+// console.log("Multiply Ans:", ans);
+
+// let squarenumber = function (num) {
+//     let ans = num**2;
+//     return ans;
+// }
+
+// let ans = squarenumber(5);
+// console.log(ans);
+
+
+// let getExp =  (x, y) => {
+//     let ans = x**y;
+//     return ans;
+// }
+
+// console.log(getExp(2, 10));
+
+
+
+
+
+

7b363809f02ba7c05ec277c946eef707e6121bb3
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-10-26 23:01:32 +0530

    learn about  Variable/Function Hoisting && Function Calls

---
+++ lecture32/index.js
@@ -0,0 +1,122 @@
+
+
+// console.log(greet);
+
+
+
+// var greet = function() {
+//     console.log("Namaste Dunia");
+// }
+
+
+
+// let obj = {
+//     age: 25,
+//     wt: 36,
+//     ht:100,
+//     greet: ()=>{
+//         console.log("Hello Dunia");
+        
+//     }
+// }
+
+// console.log(obj.greet);
+// obj.greet();
+
+
+
+
+
+// const arr = [
+//     function(a,b) {
+//         return a + b;
+//     },
+
+//     function(a,b) {
+//         return a - b;
+
+//     }, 
+
+//     function(a,b) {
+//         return a * b;
+//     }
+// ]
+
+// let first = arr[2];
+// let ans = first(5, 10);
+
+// console.log(ans);
+
+
+
+
+
+// function solve(number) {
+//     return function(number) {
+//         return number * number; 
+//     }
+// }
+
+
+// let ans = solve(5);
+
+// let finalAns = ans(10);
+// console.log(finalAns);
+
+
+
+
+
+// function greetMe(greet, fullName) {
+//     console.log("Hello", fullName);
+//     greet();
+    
+// }
+
+
+
+// function greet() {
+//     console.log("Greeting for the Day");
+    
+// }
+
+// greetMe(greet, "Aditya");
+// greet();
+
+
+
+
+// console.log(object1);
+
+// class Human {
+
+// }
+
+// const object1 = new  Human();
+
+
+
+// let sayHello = function() {
+//     console.log("Hello ji , kaise ho sarre.");
+    
+// }
+
+// sayHello();
+
+
+
+// console.log(age);
+
+// var age = 25;
+
+
+
+// sayMyName("Aditya Prabhulkar")
+
+
+// function sayMyName(finalName) {
+//     console.log(finalName);
+// }
+
+
+

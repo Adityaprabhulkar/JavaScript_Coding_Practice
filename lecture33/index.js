9bf5b88c09e78fa749ac4a23b69690789d2bfa47
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-10-27 16:16:36 +0530

    learning about temporal dead zone

---
+++ lecture33/index.js
@@ -0,0 +1,59 @@
+
+
+
+console.log(marks);
+console.log("bbbar");
+console.log("Love");
+const marks = 100;
+console.log(marks);
+
+
+
+// {
+//     const age = 15;
+//     console.log(age);
+// }
+
+// console.log(fullName);
+// function sayHello() {
+//     let fullName = "Earth";
+//     console.log("Hello Duniya ", fullName);
+    
+// }
+
+// console.log(fullName);
+
+// sayHello();
+
+
+
+
+
+
+
+//  const age = 15;
+
+
+//  console.log(age);
+
+//  {
+//     console.log(age);
+    
+//  }
+
+//  if(true){
+//     console.log(age);
+    
+//  }
+
+//  for(let i = 0;i<2;i++){
+//     console.log(age);
+    
+//  }
+
+//  function sayHello() {
+//     console.log("hii ", age);
+    
+//  }
+ 
+//  sayHello();

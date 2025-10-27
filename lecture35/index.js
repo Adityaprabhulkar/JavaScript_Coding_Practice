0cee0ddbda7ef2d42660adb12621f9c0dc278c6d
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-10-27 18:28:01 +0530

    Today learn About Common In-Built Objects in JS

---
+++ lecture35/index.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+let curr = new Date();
+
+let date = new Date('November 29 2000 07:15:30');
+
+let newDate = new Date(2000, 10, 29, 7)
+
+// console.log( newDate);
+
+// console.log(newDate.getDay());
+
+// console.log(newDate.getFullYear());
+console.log(newDate.setFullYear(1995));
+
+console.log(newDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Aditya");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(60, 30, 24, 233, 45, 46, 67, 34,222));
+
+// console.log(Math.min(60, 30, 24, 233, 45, 46, 67, 34,222));
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.floor(1.9));
+
+// console.log(Math.ceil(1.1));
+
+// console.log(Math.abs(-5));
+
+// console.log(Math.random())
+
+// console.log(Math.sqrt(5));
+
+// console.log(Math.pow(2,10));
+
+
+
+
+
+
+
+

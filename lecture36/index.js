2fc76614a022d80601a34f39f5738685dd6c1675
Aditya Prabhulkar <adityaprabhulkar2000@gmail.com>
2025-10-27 19:12:19 +0530

    Learn about Object cloning && Garbage Collector in JS

---
+++ lecture36/index.js
@@ -0,0 +1,56 @@
+// let src = {
+//     age:12,
+//     wt:90,
+//     ht:180
+// };
+
+// let dest = {};
+// //Cloning using iteration
+// for(let key in src) {
+//     let newKey = key;
+//     let newValue = src[key];
+//     // insert newKey and value in dest and create a clone
+//     dest[newKey] = newValue;
+// }
+
+// src.age = 20;
+
+// console.log("src: ", src);
+// console.log("dest: ", dest);
+
+
+
+
+
+// let src2 = {
+//     value:101,
+//     name:"Aditya Prabhulkar"
+// };
+
+// let dest = Object.assign({}, src, src2)
+
+// src.age = 20;
+
+// console.log("src: ", src);
+// console.log("dest: ", dest);
+
+
+// let dest = {...src}
+// // let dest = src;
+
+// src.age = 20;
+
+// console.log("src: ", src);
+// console.log("dest: ", dest);
+
+
+
+// console.log(obj);
+
+// obj.color = "white"
+
+// console.log(obj);
+
+
+
+
